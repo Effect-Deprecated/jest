f4174741f9f7ad18e0052b6724f44374d2c1a197
Michael Arnaldi <arnaldimichael@gmail.com>
2021-03-08 11:57:23 +0000

    filter

--- packages/jest/src/Modules/index.ts
+++ packages/jest/src/Modules/index.ts
@@ -7,15 +7,30 @@ export function tracedMapper(modules: A.Array<string>) {
   return pipe(
     modules,
     A.chain((p) => [
-      tuple(`${p}/(.*)$`, `<rootDir>/node_modules/@effect-ts/${p}/_traced/$1`),
-      tuple(`${p}$`, `<rootDir>/node_modules/@effect-ts/${p}/_traced`)
+      tuple(`${p}/(.*)$`, `<rootDir>/node_modules/${p}/_traced/$1`),
+      tuple(`${p}$`, `<rootDir>/node_modules/${p}/_traced`)
     ]),
     D.fromArray
+  )
+}
+
+export function safeMapper(modules: A.Array<string>) {
+  return pipe(
+    modules,
+    A.filter((module) => {
+      try {
+        require(module)
+        return true
+      } catch {
+        return false
+      }
+    }),
+    tracedMapper
   )
 }
 
 export const defaultMapper = pipe(
   ["system", "core", "monocle", "morphic", "query", "express", "node", "jest"],
   A.map(Str.prepend("@effect-ts/")),
-  tracedMapper
+  safeMapper
 )
