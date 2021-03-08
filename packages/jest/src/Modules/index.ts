9cb43e3fdcd3594b074f22913219f11460830e6c
Michael Arnaldi <arnaldimichael@gmail.com>
2021-03-08 11:40:31 +0000

    improve

--- packages/jest/src/Modules/index.ts
+++ packages/jest/src/Modules/index.ts
@@ -1,12 +1,21 @@
 import * as A from "@effect-ts/core/Array"
 import * as D from "@effect-ts/core/Dictionary"
+import * as Str from "@effect-ts/core/String"
 import { pipe, tuple } from "@effect-ts/system/Function"
 
-export const tracedMapper = pipe(
+export function tracedMapper(modules: A.Array<string>) {
+  return pipe(
+    modules,
+    A.chain((p) => [
+      tuple(`${p}/(.*)$`, `<rootDir>/node_modules/@effect-ts/${p}/_traced/$1`),
+      tuple(`${p}$`, `<rootDir>/node_modules/@effect-ts/${p}/_traced`)
+    ]),
+    D.fromArray
+  )
+}
+
+export const defaultMapper = pipe(
   ["system", "core", "monocle", "morphic", "query", "express", "node", "jest"],
-  A.chain((p) => [
-    tuple(`@effect-ts/${p}/(.*)$`, `<rootDir>/node_modules/@effect-ts/${p}/_traced/$1`),
-    tuple(`@effect-ts/${p}$`, `<rootDir>/node_modules/@effect-ts/${p}/_traced`)
-  ]),
-  D.fromArray
+  A.map(Str.prepend("@effect-ts/")),
+  tracedMapper
 )
