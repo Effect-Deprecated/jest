13bfef038794238c4fee903fc823b654ba599a80
Michael Arnaldi <arnaldimichael@gmail.com>
2021-05-11 22:47:52 +0100

    test(jest): test schedule

--- packages/jest/test/testing.test.ts
+++ packages/jest/test/testing.test.ts
@@ -1,7 +1,10 @@
 import * as T from "@effect-ts/core/Effect"
 import * as Ex from "@effect-ts/core/Effect/Exit"
 import * as F from "@effect-ts/core/Effect/Fiber"
+import * as Ref from "@effect-ts/core/Effect/Ref"
+import * as Sc from "@effect-ts/core/Effect/Schedule"
 import * as E from "@effect-ts/core/Either"
+import { pipe } from "@effect-ts/core/Function"
 
 import * as Te from "../src/Test"
 import { Crypto, CryptoLive, PBKDF2ConfigTest } from "./crypto"
@@ -20,6 +23,26 @@ describe("Testing", () => {
       const result = yield* _(T.either(F.join(fiber)))
 
       expect(result).toEqual(E.right(void 0))
+    }))
+
+  it("repeats", () =>
+    T.gen(function* (_) {
+      const cnt = yield* _(Ref.makeRef(0))
+
+      const fiber = yield* _(
+        pipe(
+          cnt,
+          Ref.update((n) => n + 1),
+          T.delay(1_000),
+          T.repeat(Sc.recurs(9)),
+          T.fork
+        )
+      )
+
+      yield* _(Te.adjust(10_000))
+      yield* _(F.join(fiber))
+
+      expect(yield* _(Ref.get(cnt))).toEqual(10)
     }))
 
   it("time live", () =>
